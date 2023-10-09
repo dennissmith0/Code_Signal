28449e931ee61253544ce9788d3d2f1f0306c440
Dennis <domoreburpees@icloud.com>
2023-10-09 15:42:38 -0600

    Update array_change.py

--- solutions/array_change.py
+++ solutions/array_change.py
@@ -1,15 +1,27 @@
 def solution(inputArray):
 
+# Initialize the count of moves to 0
     moves = 0
     
+    # Loop through the array starting from the second element
     for i in range(1, len(inputArray)):
+        # Get the current and previous elements
         current = inputArray[i]
         prev = inputArray[i - 1]
         
-        while current <= prev:
-            current += 1
-            moves += 1
-        
-        inputArray[i] = current
+        # Check if the current element is less than or equal to the previous element
+        if current <= prev:
+            # Calculate how much we need to increase current by
+            diff = prev - current
+            increment = diff + 1
             
+            # Increment the current element directly
+            current += increment
+            
+            # Add the increment value to the total moves
+            moves += increment
+        
+        # Update the value of the current element in the array
+        inputArray[i] = current
+    
     return moves
